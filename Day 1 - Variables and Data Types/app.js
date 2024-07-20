887efb2131fa6075d31181e6b99995d7a63d2df2
jagtapshubham <jagtapshubham190@gmail.com>
2024-07-20 18:04:21 +0530

    Day 1: Variables and Data Types

---
+++ Day 1 - Variables and Data Types/app.js
@@ -0,0 +1,37 @@
+// Activity 1: Variable Declaration
+var num = 19;
+console.log(num);
+
+let str = "Hi JavaScript";
+console.log(str);
+
+// Activity 2: Constant Declaration
+const bool = true;
+console.log(bool);
+
+// Activity 3: Data types
+const Number = 10;
+const String = "Javascript";
+const Bool = false;
+const Array = [1, 9, 4, 6];
+const Object = {
+  name: "Shubham",
+  age: "29",
+};
+
+console.log(typeof Number);
+console.log(typeof String);
+console.log(typeof Bool);
+console.log(typeof Array);
+console.log(typeof Object);
+
+// Activity 4: Reassigning Varibles
+let change = "Shub";
+console.log(change);
+change = "Max";
+console.log(change);
+
+//Activity 5: Understand const
+const value = 10;
+value = 11;
+console.log(value);
